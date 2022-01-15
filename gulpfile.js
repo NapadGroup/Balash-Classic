5135c5640d6486a0efebf70626abb84ab5cc1a24
Gnkalk <74614163+Gnkalk@users.noreply.github.com>
2022-01-15 15:12:39 +0330

    Gulp.js Setup

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const rename = require("gulp-rename");
+
+function exportcss() {
+  return gulp.src('./src/sass/**/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/css'));
+};
+
+exports.exportcss = exportcss;
+exports.watch = function () {
+  gulp.watch('./src/sass/**/*.sass', exportcss);
+};
+
+function build() {
+  return gulp.src('./src/css/**/*.css')
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(cssnano())
+    .pipe(rename(function (path) {
+      path.extname = ".min.css";
+    }))
+    .pipe(gulp.dest('./build'))
+};
+
+exports.build = build;
+
+exports.default = build;
