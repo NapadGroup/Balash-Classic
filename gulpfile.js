119a8aceb4600a38a422171db39b6be105fa74e7
Gnkalk <74614163+Gnkalk@users.noreply.github.com>
2022-02-28 19:05:45 +0330

    update gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { watch } = require('gulp');
 const gulp = require('gulp');
+const ignore = require('gulp-ignore');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
@@ -17,11 +18,18 @@ exports.watch = function () {
   gulp.watch('./src/sass/**/*.sass', exportcss);
 };
 
+buildfilter = ['form/radiobtn.css', 'form/input-box.css', 'form/text.css', 'form/textarea.css',
+               'form/toggle.css', 'form/checkbox.css', 'core/base.css', 'core/typography.css', 
+               'utilities/basic.css', 'utilities/display.css', 'utilities/div.css', 
+               'utilities/position.css', 'utilities/size.css', 'utilities/text.css'];
+
 function build() {
+
   return gulp.src('./src/css/**/*.css')
     .pipe(autoprefixer({
       cascade: false
     }))
+    .pipe(ignore.exclude(buildfilter))
     .pipe(cssnano())
     .pipe(rename(function (path) {
       path.extname = ".min.css";
