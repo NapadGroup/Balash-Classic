ca660694f731f03773a398c6a71ce339ac27d9ae
Gnkalk <74614163+Gnkalk@users.noreply.github.com>
2022-03-27 13:14:49 +0430

    update Dark

--- src/js/Smart-Dark.js
+++ src/js/Smart-Dark.js
@@ -1,8 +1,20 @@
 dark_key = document.querySelector('button[darkbtn]');
 elements = document.querySelectorAll("body *");
+root = document.querySelector(':root');
 
-dark_key.addEventListener('click', function () {
+function Chengemode() {
     for (let i = 0; i < elements.length; i++) {
         elements[i].toggleAttribute('dark');
+        if (elements[i].hasAttribute('dark')) {
+            root.style.cssText += '--bg-primary-color: var(--bg-dark-primary-color);\n--bg-secondary-color: var(--bg-dark-secondary-color);';
+        } else {
+            root.style.cssText += '--bg-primary-color: var(--bg-light-primary-color);\n--bg-secondary-color: var(--bg-light-secondary-color);';
+        }
     }
-});
+}
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    Chengemode();
+}
+
+dark_key.addEventListener('click', Chengemode);
