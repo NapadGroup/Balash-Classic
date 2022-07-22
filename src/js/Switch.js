c331daa58d9fd6c52fc2bece4beefd7769281e09
Gnkalk <74614163+Gnkalk@users.noreply.github.com>
2022-07-22 15:38:40 +0430

    Update Switch

--- src/js/Switch.js
+++ src/js/Switch.js
@@ -8,9 +8,14 @@ if (Switchs.length > 0 && ContentS.length == Switchs.length) {
         const Content = document.querySelector(`contents[name="${ContentName}"]`);
         const Switch_keys = Switch.children;
         const Default_key = Switch.getAttribute('default') - 1;
+        if (sessionStorage.getItem(`ActiveKey${i}`) == null) {
+            sessionStorage.setItem(`ActiveKey${i}`, Default_key)
+        }
+        const ak = i;
         const Contents = Content.children;
-        for (let i = 0; i < Switch_keys.length; i++) {
-            Switch_keys[i].addEventListener('click', function () {
+        for (let z = 0; z < Switch_keys.length; z++) {
+            Switch_keys[z].addEventListener('click', function () {
+                sessionStorage.setItem(`ActiveKey${ak}`, z)
                 for (let j = 0; j < Switch_keys.length; j++) {
                     Switch_keys[j].removeAttribute('active');
                 }
@@ -18,11 +23,12 @@ if (Switchs.length > 0 && ContentS.length == Switchs.length) {
                 for (let k = 0; k < Contents.length; k++) {
                     Contents[k].removeAttribute('active');
                 }
-                Contents[i].setAttribute('active', true);
+                Contents[z].setAttribute('active', true);
             });
         }
-        Switch_keys[Default_key].setAttribute('active', true);
-        Contents[Default_key].setAttribute('active', true);
+        const ActiveKey = sessionStorage.getItem(`ActiveKey${i}`);
+        Switch_keys[ActiveKey].setAttribute('active', true);
+        Contents[ActiveKey].setAttribute('active', true);
     }
 } else {
     console.error('Switchs and Contents are not equal');
